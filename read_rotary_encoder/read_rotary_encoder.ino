cd62b61f28963b51ef7356381575974e1afebc13
raphaFanti <raphael.fanti@gmail.com>
2021-03-05 13:47:40 +0100

    Implemented rotary encoder reading

--- read_rotary_encoder/read_rotary_encoder.ino
+++ read_rotary_encoder/read_rotary_encoder.ino
@@ -1,6 +1,28 @@
+/*
+Oberalp - R&I Lab
+Logging ski scanner's data
+
+By: Raphael Fanti
+Date: MArch 5th, 2021
+
+Setup:
+ - Ski scanner shield on Adalogger
+ - rotary encoder on port rot1 (closest to led)
+ 
+*/
+
 // include libraries needed for Adalogger
 #include <SPI.h> //lib for serial peripheral interface
 #include <SD.h> //lib for sd card
+
+
+// rotary encoder on port rot1
+const int rot1APin = 6;
+const int rot1BPin = 9;
+long rot1Pulses = 0; // counter of pulses on rot1
+const int rot1PulsesPerRev = 500;
+long rot1Pos = 0; // angle on rot1 (deg)
+
 
 // leds and button pin declaration
 const int builtinLedPin = 13; // red led onboard (used as standby indicator)
@@ -13,7 +35,6 @@ bool oldRecording = false;
 
 // runtime choices
 const bool printSerial = true;
-const bool writeSD = false;
 
 //chipset declaration for Adalogger / sd card
 const int chipSelect = 4;
@@ -26,33 +47,44 @@ unsigned long lastButtonPress = millis();
 void setup() {
 
   // start serial comm
-  Serial.begin(9600); // start serial
+  if (printSerial == true){
+    Serial.begin(9600); // start serial
+  }
   
   // pin mode declaration
+  pinMode(rot1APin, INPUT_PULLUP);
+  pinMode(rot1BPin, INPUT_PULLUP);
+  
   pinMode(builtinLedPin, OUTPUT);
   pinMode(externalLedPin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
 
   // interruption declarations
   attachInterrupt(digitalPinToInterrupt(buttonPin), recordingChange, FALLING); // falling because of pullup
-
-  // 
+  attachInterrupt(digitalPinToInterrupt(rot1APin), rot1Pulse, FALLING);
 
 }
 
 void loop() {
+
+  // update encoders position
+  rot1Pos = updateRot(rot1Pulses, rot1PulsesPerRev);
   
   // detect recording button push actioned
   if(recording != oldRecording){
 
     // understands if at beginning or end of recording
     if (recording == HIGH){ // beginning
+
+      // create file (TBD)
       
       // recording light on, standby off
       digitalWrite(externalLedPin, HIGH);
       digitalWrite(builtinLedPin, LOW);
       
     } else{ // end
+
+      // close file (TBD)
 
       // recording light off, standby on
       digitalWrite(externalLedPin, LOW);
@@ -64,11 +96,24 @@ void loop() {
     
   }
 
-  // prints serial
-  Serial.println(recording);
+  
+  
+
+  if (printSerial == true){
+    Serial.println(rot1Pos);
+  }
 
   delay(200);
 
+}
+
+void rot1Pulse(){
+  if(digitalRead(rot1BPin) == HIGH) rot1Pulses += 1;
+  else rot1Pulses -= 1;
+}
+
+long updateRot(long pulses, float ppr){
+  return pulses * 360.0 / ppr;
 }
 
 void recordingChange() {
